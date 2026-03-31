106b7e5024fabeeca2e404632bb57abb13808237
lorenzo <rexzaghenyt@gmail.com>
2026-03-31 16:11:00 +0200

    aggiunta di connessione a mysql + database e signal_handler

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
+import mysql.connector
+import signal
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
 import logging
+import sys
 
 load_dotenv()
 TOKEN=os.getenv('DISCORD_TOKEN')
@@ -13,6 +16,20 @@ intents.message_content = True
 intents.members = True
 
 bot=commands.Bot(command_prefix='/',intents=intents)
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'user',
+    passwd = 'password'
+
+)
+
+def signal_handler(sig, frame):
+    dataBase.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 
 @bot.event
 async def on_ready():
